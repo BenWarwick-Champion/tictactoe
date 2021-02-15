46b2fdc76b428e1db56f33e172fc3c57e70f090e
Ben Warwick-Champion <ben.w-c@sky.com>
2021-02-15 13:05:19 +0000

    Added more board states to winner function test

--- winner_test.py
+++ winner_test.py
@@ -1,14 +1,33 @@
-
 from tictactoe import X, O, EMPTY, winner
 
-
 if __name__=="__main__":
-    board = [
-        [EMPTY, X, EMPTY],
-        [EMPTY, X, O],
-        [EMPTY, X, O]
+    boards = [
+        [
+            [EMPTY, X, EMPTY],
+            [EMPTY, X, O],
+            [EMPTY, X, O]
+        ],
+        [
+            [X, EMPTY, EMPTY],
+            [O, O, O],
+            [X, X, EMPTY]
+        ],
+        [
+            [O, X, X],
+            [X, O, X],
+            [X, X, O]
+        ],
+        [
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]
+        ]
+
     ]
 
-    result = winner(board)
-    assert result == X, f"Winner returned {result}. Expected 'X'."
+    answers = [X, O, O, None]
 
+    for ind, board in enumerate(boards):
+        assert winner(board) == answers[ind], f"Winner returned {winner(board)}. Expected {answers[ind]}."
+
+    print("Tests complete.")
